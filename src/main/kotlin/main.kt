75fb113417854b538221ee91dc7d159eb17b7809
apexnt <apexnt@yandex.ru>
2022-03-08 12:36:41 +0500

    rework

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,47 +1,51 @@
-import kotlin.system.exitProcess
-
 const val PRICE_UNIT = 1000u
-const val DISCOUNT_1LV = 10u
-const val DISCOUNT_2LV = 5u
+const val DISCOUNT_1LEVEL = 100u
+const val DISCOUNT_2LEVEL = 5u
+const val TEXT_AMOUNT_TO_BE_PAID = "Сумма к оплате"
+const val TEXT_RUB = "руб."
 
 fun main() {
-    val amountPriceUser: UInt
-    val amountPriceUser2lv: UInt
-    val amountPriceVipUser: UInt
+    val totalAmount: UInt
+    val subTotalVipUser: UInt
 
     println("Добро пожаловать в магазин музыкальных альбомов МЕЛОДИЯ !")
-    println("Стоимость одного альбома $PRICE_UNIT руб.")
+    println("Стоимость одного альбома $PRICE_UNIT $TEXT_RUB")
     print("Введите желаемое количество альбомов для покупки: ")
     val amountMelodies = readLine()?.toUInt() ?: return
-
+    val subTotal = PRICE_UNIT * amountMelodies
     print("Сумма ваших покупок в прошлый месяц?: ")
     val amountPreviousPeriod = readLine()?.toUInt() ?: return
-
-    if (amountPreviousPeriod <= 1_000u) {
-        amountPriceUser = (PRICE_UNIT * amountMelodies)
-        print("Сумма к оплате $amountPriceUser руб.")
-        exitProcess(0)
-    } else if (amountPreviousPeriod in 1_001u..10_000u) {
-        amountPriceUser = ((PRICE_UNIT * amountMelodies) - DISCOUNT_1LV)
-        println("Сумма к оплате: $amountPriceUser руб., скидка составила: $DISCOUNT_1LV руб. ")
-        exitProcess(0)
-    } else {
-        print("Вы постоянный клиент? y - да, n - нет.")
-    }
+    print("Вы постоянный клиент? y - да, n - нет.")
     val vipUser: Boolean
     val checkVipUser = readLine()
     vipUser = checkVipUser == "y"
 
-    if (amountPreviousPeriod > 10_000u && !vipUser) {
-        val discountUser = ((PRICE_UNIT * amountMelodies) * DISCOUNT_2LV / 100u)
-        amountPriceUser2lv = (PRICE_UNIT * amountMelodies) - discountUser
-        print("Сумма к оплате: $amountPriceUser2lv руб., скидка составила: $discountUser руб. ")
-
+    if (amountPreviousPeriod <= 1_000u && !vipUser) {
+        totalAmount = subTotal
+        print("$TEXT_AMOUNT_TO_BE_PAID $totalAmount $TEXT_RUB")
+        return
+    } else if (amountPreviousPeriod <= 1_000u && vipUser) {
+        totalAmount = subTotal - (subTotal / 100u)
+        println("$TEXT_AMOUNT_TO_BE_PAID $totalAmount $TEXT_RUB")
+        return
+    } else if (amountPreviousPeriod in 1_001u..10_000u && !vipUser) {
+        totalAmount = subTotal - DISCOUNT_1LEVEL
+        println("$TEXT_AMOUNT_TO_BE_PAID $totalAmount $TEXT_RUB")
+        return
+    } else if (amountPreviousPeriod in 1_001u..10_000u && vipUser) {
+        subTotalVipUser = subTotal - DISCOUNT_1LEVEL
+        totalAmount = subTotalVipUser - (subTotalVipUser / 100u)
+        println("$TEXT_AMOUNT_TO_BE_PAID $totalAmount $TEXT_RUB")
+        return
+    } else if (amountPreviousPeriod > 10_000u && !vipUser) {
+        totalAmount = subTotal - (subTotal * DISCOUNT_2LEVEL / 100u)
+        println("$TEXT_AMOUNT_TO_BE_PAID $totalAmount $TEXT_RUB")
+        return
     } else if (amountPreviousPeriod > 10_000u && vipUser) {
-        val discountVipUser =
-            ((PRICE_UNIT * amountMelodies) * DISCOUNT_2LV / 100u) + ((PRICE_UNIT * amountMelodies) / 100u)
-        amountPriceVipUser = (PRICE_UNIT * amountMelodies) - discountVipUser
-        print("Сумма к оплате: $amountPriceVipUser руб., скидка составила: $discountVipUser руб. ")
+        subTotalVipUser = subTotal - (subTotal * DISCOUNT_2LEVEL / 100u)
+        totalAmount = subTotalVipUser - (subTotalVipUser / 100u)
+        println("$TEXT_AMOUNT_TO_BE_PAID $totalAmount $TEXT_RUB")
+        return
     }
 }
 
