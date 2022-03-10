15e346e1b44997ad2d198684121155fd90d86393
apexnt <apexnt@yandex.ru>
2022-03-10 23:17:27 +0500

    rework5

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,21 +2,7 @@ const val PRICE_UNIT = 1000u
 const val DISCOUNT_1LEVEL = 10u
 const val DISCOUNT_2LEVEL = 5u
 
-fun discount(amountPreviousPeriod: UInt, subTotal: UInt): UInt {
-    var discountAmount = 0u
-    if (amountPreviousPeriod <= 1_000u) discountAmount = subTotal
-    else if (amountPreviousPeriod in 1_001u..10_000u) discountAmount = subTotal - DISCOUNT_1LEVEL
-    else if (amountPreviousPeriod > 10_000u) discountAmount = subTotal - (subTotal * DISCOUNT_2LEVEL / 100u)
-    return discountAmount
-}
-
-fun discountVip(discountAmount: UInt, vipUser: Boolean): UInt {
-    return if (vipUser) discountAmount - (discountAmount / 100u)
-    else discountAmount
-}
-
 fun main() {
-
     println("Добро пожаловать в магазин музыкальных альбомов МЕЛОДИЯ !")
     println("Стоимость одного альбома $PRICE_UNIT руб.")
     print("Введите желаемое количество альбомов для покупки: ")
@@ -31,5 +17,17 @@ fun main() {
     val discountAmount = discount(amountPreviousPeriod, subTotal)
     val vipUserAmount = discountVip(discountAmount, vipUser)
     println("Сумма к оплате: $vipUserAmount руб.")
+}
 
+fun discount(amountPreviousPeriod: UInt, subTotal: UInt): UInt {
+    var discountAmount = 0u
+    if (amountPreviousPeriod <= 1_000u) discountAmount = subTotal
+    else if (amountPreviousPeriod in 1_001u..10_000u) discountAmount = subTotal - DISCOUNT_1LEVEL
+    else if (amountPreviousPeriod > 10_000u) discountAmount = subTotal - (subTotal * DISCOUNT_2LEVEL / 100u)
+    return discountAmount
+}
+
+fun discountVip(discountAmount: UInt, vipUser: Boolean): UInt {
+    return if (vipUser) discountAmount - (discountAmount / 100u)
+    else discountAmount
 }
